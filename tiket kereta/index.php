bb440f9bbf58e5b61ad3d22445343ff6cbce08d2
dimaswildan28rpl <61201024+dimaswildan28rpl@users.noreply.github.com>
2020-03-30 01:59:52 +0700

    Add files via upload

---
+++ tiket kereta/index.php
@@ -0,0 +1,30 @@
+
+
+<title>Halaman Sukses Login</title>
+<!-- <div align='center'>
+   Selamat Datang, <b><?php echo $username;?></b> <a href="logout.php"><b>Logout</b></a>
+</div> -->
+<html>
+<head>
+    <title>navigasi menu</title>
+    <link rel="stylesheet" href="navbar.css">
+    </head>
+    <header>
+        <div class="container">
+          <div class="logo-box">
+              <img src="ap.png"  height="80"width="200">
+            </a>
+          </div>
+          <nav>
+          <ul>
+            <li><a href="">home</a></li>
+            <li><a href="">login</a></li>
+            <li><a href="">register</a></li>
+            <li><a href="http://localhost/tiket%20kereta/longout.php">longout</a></li>
+            <li><a href="">contact</a></li>
+         </ul>
+        </nav>
+        </div>
+      </header>
+      
+</html>
